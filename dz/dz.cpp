0c4ce0d2528016fb7f17ea46f9a54e1913b98f3f
Yakim_jh34 <Yakim_jh34@21-12>
2024-11-06 18:39:44 +0200

    Add project files.

---
+++ dz/dz.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    //task1
+    //cout << "\"Yakimovich\"\n\"Mikolay\"\n\"Volodimyrovich\"";
+
+    //task2
+    //cout << " \"I didn't\n\t   say\n\t\tthat\"\n\t\t\t\\Jason Statham\\";
+}
